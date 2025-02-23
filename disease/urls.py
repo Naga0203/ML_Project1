91dcc7f19abf54abe528b8e60e2d4d83e1e46c92
Naga durgarao Thuttupu <nagadurgarao524@gmail.com>
2025-02-23 09:03:45 +0530

    Update urls.py

--- disease/urls.py
+++ disease/urls.py
@@ -4,5 +4,6 @@ from .views import get_predictions, pred , features
 urlpatterns = [
     path('', pred, name='home'),
     path('features/' , features , name='features'),
+    path('home/',pred , name='home'),
     path('result', get_predictions, name='result'),
 ]
